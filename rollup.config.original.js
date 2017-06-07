3787b948c590022f281f80962306af0f24cf58d4
Kurt Milam <kurt.milam@xioup.com>
2017-06-07 23:14:43 +0200

    Add auth0-js, create auth component and apply fixes in rollup.config.js
    1. Installed rollup-plugin-babel and added it to rollup.config.js
    2. Added necessary replace() calls for superagent / formidable in
       rollup.config.js
    3. Created auth0 component at:
       src/public/components/auth0.js
    4. Imported the auth0 component into:
       src/public/pages/main.js

--- rollup.config.original.js
+++ rollup.config.original.js
@@ -3,7 +3,6 @@ import builtins    from "rollup-plugin-node-builtins"
 import commonjs    from "rollup-plugin-commonjs"
 import nodeResolve from "rollup-plugin-node-resolve"
 import replace     from "rollup-plugin-replace"
-import json        from "rollup-plugin-json"
 import uglify      from "rollup-plugin-uglify"
 
 export default {
@@ -12,15 +11,9 @@ export default {
       "global.setTimeout": "window.setTimeout",
       "global.clearTimeout": "window.clearTimeout",
       "global.performance": "window.performance",
-      "process.env.NODE_ENV": JSON.stringify( process.env.NODE_ENV ),
-      "global.GENTLY": "false" // this line is for auth0-js / formidable
-    } ),
-    replace( { // this replace was added for auth0-js / superagent
-      include: '**/superagent/lib/request-base.js',
-      values: { ' clearTimeout': ' window.clearTimeout' }
-    } ),
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
+    }),
     builtins(),
-    json(), // this plugin was added for auth0-js
     nodeResolve(),
     commonjs({
       include: 'node_modules/**',
@@ -35,7 +28,6 @@ export default {
         "node_modules/kefir/dist/kefir.js": [
           "Observable",
           "Property",
-          "Stream",
           "combine",
           "concat",
           "constant",
