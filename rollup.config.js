1719cd1fdb9c24041c1f0d74d14a7fc2d0efd1bd
phil olson <philip.olson@protonmail.ch>
2017-06-21 23:40:50 -0500

    fix bug and remove unnecessary code (as a result of bug fix)

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,7 @@
 import babel       from "rollup-plugin-babel"
-import builtins    from "rollup-plugin-node-builtins"
 import commonjs    from "rollup-plugin-commonjs"
 import nodeResolve from "rollup-plugin-node-resolve"
 import nodeGlobals from "rollup-plugin-node-globals"
-import replace     from "rollup-plugin-replace"
 import json        from "rollup-plugin-json"
 
 // NOTE: I have commented out a few lines in this file.
@@ -12,19 +10,7 @@ import json        from "rollup-plugin-json"
 
 export default {
   plugins: [
-    replace({
-      "global.setTimeout": "window.setTimeout",
-      "global.clearTimeout": "window.clearTimeout",
-      "global.performance": "window.performance",
-      "process.env.NODE_ENV": JSON.stringify( process.env.NODE_ENV )
-    } ),
-    replace( {
-      include: '**/node_modules/formidable/lib/**',
-      values: { 'global.GENTLY': 'false'
-              } // added for formidable (auth0 / superagent dependency)
-    } ),
     json(), // this plugin was added for auth0-js
-    builtins(),
     nodeResolve(),
     commonjs({
       include: 'node_modules/**',
